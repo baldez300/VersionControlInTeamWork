52aad0b7fde0e47fef923183efe63c5e0b4da6d9
Balde641 <62099893+Balde641@users.noreply.github.com>
2023-09-15 15:08:58 +0300

    Checking out feature

--- src/Task4_1/GreetingMachine.java
+++ src/Task4_1/GreetingMachine.java
@@ -15,6 +15,6 @@ public class GreetingMachine {
         gm.greet();
         gm.greet("Good morning Universe");
 
-        System.out.println("Removing all whitespaces from the code...");
+        System.out.println("Removing all whitespaces ");
     }
 }
