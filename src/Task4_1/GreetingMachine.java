8eb4ca3fedb94c4590b3735f673198ae59f6b689
Balde641 <62099893+Balde641@users.noreply.github.com>
2023-09-15 10:10:33 +0300

    checking branches work

--- src/Task4_1/GreetingMachine.java
+++ src/Task4_1/GreetingMachine.java
@@ -3,7 +3,7 @@ package Task4_1;
 public class GreetingMachine {
 
     public void greet() {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! from GreetingMachine");
     }
 
     public static void main(String[] args) {
