2141793d86fa7036a9da7b6df57761e176e3bc5f
Balde641 <62099893+Balde641@users.noreply.github.com>
2023-09-15 10:06:14 +0300

    Initial commit

---
+++ src/Task4_1/GreetingMachine.java
@@ -0,0 +1,13 @@
+package Task4_1;
+
+public class GreetingMachine {
+
+    public void greet() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        GreetingMachine gm = new GreetingMachine();
+        gm.greet();
+    }
+}
