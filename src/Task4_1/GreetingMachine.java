5c23c90792670122c02c0f0c24527535577a6480
Balde641 <62099893+Balde641@users.noreply.github.com>
2023-09-17 11:34:52 +0300

    Checking which branch this is on.

--- src/Task4_1/GreetingMachine.java
+++ src/Task4_1/GreetingMachine.java
@@ -15,6 +15,6 @@ public class GreetingMachine {
         gm.greet();
         gm.greet("Good morning Universe");
 
-        System.out.println("Removing all whitespaces ");
+        System.out.println("Checking which branch this is on.");
     }
 }
